c5ab84faf6059b9ae40b6e278de59e538b3fee77
LeandroAvilaTapia <leandro.avila.tapia@gmail.com>
2023-08-04 02:19:44 -0300

    se completo los puntos de la task1

--- src/main/java/com/mindhub/homebankingAP/HomebankingApApplication.java
+++ src/main/java/com/mindhub/homebankingAP/HomebankingApApplication.java
@@ -1,7 +1,11 @@
 package com.mindhub.homebankingAP;
 
+import com.mindhub.homebankingAP.models.Client;
+import com.mindhub.homebankingAP.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApApplication {
@@ -10,4 +14,11 @@ public class HomebankingApApplication {
 		SpringApplication.run(HomebankingApApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+			clientRepository.save(new Client("Melba","Morel","melba@mindhub.com"));
+			clientRepository.save(new Client("Leandro","Avila","leandroavila@mindhub.com"));
+		});
+	}
 }
--- src/main/java/com/mindhub/homebankingAP/models/Client.java
+++ src/main/java/com/mindhub/homebankingAP/models/Client.java
@@ -1,21 +1,30 @@
-package com.mindhub.homebankingAP;
+package com.mindhub.homebankingAP.models;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Client {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private long id;
     private String firstName;
     private String lastName;
+    private String email;
 
     public Client(){}
 
-    public Client(String firstName, String lastName) {
+    public Client(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
     }
+
 
     public String getFirstName() {
         return firstName;
@@ -31,6 +40,14 @@ public class Client {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String toString() {
