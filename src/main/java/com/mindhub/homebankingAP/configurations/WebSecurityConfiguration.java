00e7dba29a87eb83790befbead66e4c01a650e63
Leandro Avila <leandro.avila.tapia@gmail.com>
2023-08-18 14:04:56 -0300

    se agrego las clases web security,authen y authon

--- src/main/java/com/mindhub/homebankingAP/configurations/WebSecurityConfiguration.java
+++ src/main/java/com/mindhub/homebankingAP/configurations/WebSecurityConfiguration.java
@@ -3,21 +3,25 @@ package com.mindhub.homebankingAP.configurations;
 import com.mindhub.homebankingAP.models.Client;
 import com.mindhub.homebankingAP.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
-
-
     @Autowired
-
     ClientRepository clientRepository;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
